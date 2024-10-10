926b2586ed64fbfeef6ae8f9c6fff2c1752872c4
Leonardo <leonardo.stefanelli2@studenti.unimi.it>
2024-10-10 16:26:14 +0200

    feat: the main now builds stars

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import utils
 import sys
 import dynamics
 
-system = utils.System('CuNP.xyz')
+system = utils.System('Cu675.xyz')
 parameters = utils.SimulationParameters('parameters.toml')
+
+# termalize
+parameters.steps = 1000
+dynamics.langevin(system, parameters)
 
 subsystem_indices = list(range(len(system.atoms)))
 
-tips = 7
+parameters.steps = 300
+tips = 5
 for i in range(parameters.n_atoms):
     radius = system.max_distance_from(system.center_of_mass(subsystem_indices))
     
@@ -17,12 +22,18 @@ for i in range(parameters.n_atoms):
             position = utils.spherical_to_cartesian(radius + parameters.radius_offset, np.pi / 2, phi) + system.center_of_mass(subsystem_indices)
             system.add_atom(1.5 * parameters.temperature, system.center_of_mass(subsystem_indices), position)
 
+    for j in range(2):
+            theta = np.pi * j
+            position = utils.spherical_to_cartesian(radius + parameters.radius_offset, theta, 0) + system.center_of_mass(subsystem_indices)
+            system.add_atom(1.5 * parameters.temperature, system.center_of_mass(subsystem_indices), position)
+
     count = 0
     while system.max_atom_separation() > 3.7:
         dynamics.langevin(system, parameters)
         count += 1
         if count > 10:
             system.view_trajectory()
+            system.save_trajectory('traj.xyz')
             print('probably detached atom')
             sys.exit()
     print(i)
